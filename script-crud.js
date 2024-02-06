d68afbd77334aa5280df076cd01b8ac19dca83a4
Bruno Lins Maia <brunolinsm@gmail.com>
2024-02-06 18:22:15 -0300

    03. Editando tarefas

--- script-crud.js
+++ script-crud.js
@@ -2,13 +2,25 @@ const botaoAddTarefa = document.querySelector('.app__button--add-task')
 const formularioAddTarefa = document.querySelector('.app__form-add-task')
 const areaTexto = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const botaoCancelarFormulario = document.querySelector('.app__form-footer__button--cancel')
 
 //Esta declaracao substitui uma função condicional
 const listaTarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
-botaoAddTarefa.addEventListener('click', () =>{
+const limparEsconderFormulario = () => {
+    areaTexto.value = ''
+    formularioAddTarefa.classList.toggle('hidden')
+}
+
+function atualizarTarefas(){
+    localStorage.setItem('tarefas', JSON.stringify(listaTarefas))
+}
+
+botaoAddTarefa.addEventListener('click', () => {
     formularioAddTarefa.classList.toggle('hidden')
 })
+
+botaoCancelarFormulario.addEventListener('click', limparEsconderFormulario)
 
 formularioAddTarefa.addEventListener('submit', function(evento) {
     evento.preventDefault()
@@ -17,7 +29,7 @@ formularioAddTarefa.addEventListener('submit', function(evento) {
     listaTarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-    localStorage.setItem('tarefas', JSON.stringify(listaTarefas))
+    atualizarTarefas()
     //Textarea se refere ao campo de escrita do formulario
     areaTexto.value = ''
     formularioAddTarefa.classList.add('hidden')
@@ -35,12 +47,23 @@ function criarElementoTarefa(tarefa){
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
     </svg>`
 
+    //Item que recebe a descrição da tarefa
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('app__section-task-list-item-description')
     paragrafo.textContent = tarefa.descricao
 
     const botaoDoItemTarefas = document.createElement('button')
     botaoDoItemTarefas.classList.add('app_button-edit')
+    botaoDoItemTarefas.onclick = () => {
+        // debugger
+        const novaDescricaoTarefa = prompt('Qual é o nome da nova tarefa?')
+        // console.log('Nova descriçãoda tarefa: ', novaDescricaoTarefa)
+        if(novaDescricaoTarefa){
+            paragrafo.textContent = novaDescricaoTarefa
+            tarefa.descricao = novaDescricaoTarefa
+            atualizarTarefas()
+        }
+    }
     
     const imagemBotao = document.createElement('img')
     imagemBotao.setAttribute('src','/imagens/edit.png')
