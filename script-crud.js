baae33815fe22e7d3b13b72912bf7aecadea2c6b
Bruno Lins Maia <brunolinsm@gmail.com>
2024-02-05 21:47:44 -0300

    Adicionando tarefas na localStorage

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,15 @@
 const botaoAddTarefa = document.querySelector('.app__button--add-task')
 const formularioAddTarefa = document.querySelector('.app__form-add-task')
+const areaTexto = document.querySelector('.app__form-textarea')
+listaTarefas = []
 
 botaoAddTarefa.addEventListener('click', () =>{
     formularioAddTarefa.classList.toggle('hidden')
+})
+
+formularioAddTarefa.addEventListener('submit', function(evento) {
+    evento.preventDefault()
+    const tarefa = {descricao: areaTexto.value}
+    listaTarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(listaTarefas))
 })
